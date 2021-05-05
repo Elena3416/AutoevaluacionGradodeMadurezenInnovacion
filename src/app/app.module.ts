411aaabafae9b18ffa976d27cd1f88810290e91d
Elena3416 <lauelena_14@hotmail.com>
2021-05-05 11:25:05 -0500

    PaginaInicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatIconModule } from "@angular/material/icon";
+import { PaginaInicialComponent } from './Component/pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
